b27783a2f10bd3c36471ebb6104cb066b549825f
adfolk <adfolkestad@gmail.com>
2025-04-23 09:05:26 +0400

    cleanInput implementation

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	results := strings.Fields(strings.ToLower(text))
+	return results
 }
